d8d8d197eec625a9b6e4b8b98c9b169998700dc3
callmesun <callme.sun@qq.com>
2012-08-06 15:55:37 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jl0432Jie::Application.config.secret_token = '17f170bc921cd9d114a9a6148922f77e9d443ef8446c8897fd93e9daa715c616befb2fc115ea70695501e118dd02823640c762764efd16b05627682660c48369'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jl0432Jie::Application.config.session_store :cookie_store, key: '_jl-0432-jie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jl0432Jie::Application.config.session_store :active_record_store
