b48f00f0ffac0d90254871fc61a7864d958f8d5d
callmesun <callme.sun@qq.com>
2012-08-13 16:43:32 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Jl0432Jie::Application.routes.draw do
+
+
+
+
+  get "test/index"
+
+  get "home/index"
+
+  resources :share_type
+
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +60,7 @@ Jl0432Jie::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
